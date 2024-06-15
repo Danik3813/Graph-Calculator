dfbdaefc2d0c111518b4696d1f4be3cdd458ddd8
Даниил Купцов <danilkup23@yandex.ru>
2024-06-15 22:37:36 +0400

    Bug fixes

--- calc.h
+++ calc.h
@@ -10,3 +10,35 @@ public:
 	T Exponentation(T a, T b);
 };
 
+//Реализация класса в заголовочном файле по причине невозможности вынести в .cpp-файл
+
+template <typename T>
+T Calc<T>::Sum(T a, T b)
+{
+	return a + b;
+}
+
+template<typename T>
+T Calc<T>::Difference(T a, T b)
+{
+	return a - b;
+}
+
+template<typename T>
+T Calc<T>::Product(T a, T b)
+{
+	return a * b;
+}
+
+template<typename T>
+T Calc<T>::Division(T a, T b)
+{
+	if (b) return a / b;
+	throw "Division by zero!";
+}
+
+template<typename T>
+T Calc<T>::Exponentation(T a, T b)
+{
+	return pow(a, b);
+}
